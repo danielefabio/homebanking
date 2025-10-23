6f91342281b49d4652446b24da4458f5f4269707
Daniele Fabio <79114079+danielefabio@users.noreply.github.com>
2025-10-23 21:47:14 +0200

    Configured Vite HMR for Docker and WSL - Added Vite service into docker-compose - Updated README (#3)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import './bootstrap';
+
+//console.log('Hellooooo Vite + Laravel!');
+//console.log('Hellooooo Vite + Laravel!');
+
+
+function hello(option = "") {
+    console.log(`Hello ${option} from the hello function!`);
+}
+
+hello();
+hello('Daniele');
+
+
+function printMessage(message) {
+    console.log(`Message: ${message}`);
+}
+
+//printMessage('ciao');
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    \Log::info('Welcome page accessed');
     return view('welcome', [
         'app_name' => config('app.name'),
     ]);
